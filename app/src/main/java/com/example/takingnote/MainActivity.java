b7136f2b05cd3a9ab77593212fe74576cce0234d
Mustafa <info.mkurt@gmail.com>
2020-12-16 22:21:22 +0300

    LiveData is done.

--- app/src/main/java/com/example/takingnote/MainActivity.java
+++ app/src/main/java/com/example/takingnote/MainActivity.java
@@ -1,14 +1,39 @@
 package com.example.takingnote;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.example.takingnote.model.Note;
+import com.example.takingnote.viewmodel.NoteViewModel;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private NoteViewModel noteViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        noteViewModel = new ViewModelProvider(this,
+                ViewModelProvider
+                        .AndroidViewModelFactory
+                        .getInstance(this.getApplication()))
+                .get(NoteViewModel.class);
+
+        noteViewModel.getAllNotes().observe(this, new Observer<List<Note>>() {
+            @Override
+            public void onChanged(List<Note> notes) {
+                // update RecyclerView
+                Toast.makeText(MainActivity.this, "onChanged", Toast.LENGTH_SHORT)
+                        .show();
+            }
+        });
     }
 }
